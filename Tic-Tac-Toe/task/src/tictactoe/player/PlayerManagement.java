6f9f57e21884ebd6744ea9113f821de95350d6c1
forynski <65871510+forynski@users.noreply.github.com>
2021-03-05 10:50:41 +0000

    Update PlayerManagement.java

--- Tic-Tac-Toe/task/src/tictactoe/player/PlayerManagement.java
+++ Tic-Tac-Toe/task/src/tictactoe/player/PlayerManagement.java
@@ -1,2 +1,43 @@
-package tictactoe.player;public class PlayerManagement {
+package tictactoe.player;
+
+import tictactoe.TicTacToeSymbol;
+
+import java.awt.*;
+
+public class PlayerManagement {
+    private int counter = 0;
+    private Player player1;
+    private Player player2;
+
+    public PlayerManagement() {
+        player1 = new HumanPlayer(TicTacToeSymbol.X);
+        player2 = new HumanPlayer(TicTacToeSymbol.O);
+    }
+
+    public PlayerManagement(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public Player getCurrentPlayer() {
+        Player player = player1;
+        if (counter == 1) {
+            player = player2;
+        }
+        return player;
+    }
+
+    public void nextPlayer() {
+        counter = ++counter % 2;
+    }
+
+    public Point nextPlayersTurn() {
+        nextPlayer();
+        return currentPlayersTurn();
+    }
+
+    public Point currentPlayersTurn() {
+        return getCurrentPlayer().choosePosition();
+    }
+
 }
