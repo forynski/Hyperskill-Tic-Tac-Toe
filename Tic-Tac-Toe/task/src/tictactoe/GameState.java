2f7f09fa1ae63e65861b6b44c5b1238613b55f7e
forynski <65871510+forynski@users.noreply.github.com>
2021-03-05 10:49:41 +0000

    Update GameState.java

--- Tic-Tac-Toe/task/src/tictactoe/GameState.java
+++ Tic-Tac-Toe/task/src/tictactoe/GameState.java
@@ -1,2 +1,10 @@
-package tictactoe;public class GameState {
+package tictactoe;
+
+public enum GameState {
+    X_WIN("X wins"), O_WIN("O wins"), DRAW("Draw"), IMPOSSIBLE("Impossible"), UNFINISHED("Game not finished");
+    public final String label;
+
+    private GameState(String label) {
+        this.label = label;
+    }
 }
