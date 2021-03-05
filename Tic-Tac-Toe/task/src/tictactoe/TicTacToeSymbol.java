e41a8ba8968475d4cad0d94e6e610fa61381c908
forynski <65871510+forynski@users.noreply.github.com>
2021-03-05 10:50:03 +0000

    Update TicTacToeSymbol.java

--- Tic-Tac-Toe/task/src/tictactoe/TicTacToeSymbol.java
+++ Tic-Tac-Toe/task/src/tictactoe/TicTacToeSymbol.java
@@ -1,2 +1,10 @@
-package tictactoe;public class TicTacToeSymbol {
+package tictactoe;
+
+public enum TicTacToeSymbol {
+    X('X'), O('O'), U(' ');
+    public final char label;
+
+    private TicTacToeSymbol(char label) {
+        this.label = label;
+    }
 }
