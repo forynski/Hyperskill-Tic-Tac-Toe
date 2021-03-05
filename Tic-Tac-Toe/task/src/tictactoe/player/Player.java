ad8adbafaff1726053ab44f9a10175964019a63b
forynski <65871510+forynski@users.noreply.github.com>
2021-03-05 10:50:26 +0000

    Update Player.java

--- Tic-Tac-Toe/task/src/tictactoe/player/Player.java
+++ Tic-Tac-Toe/task/src/tictactoe/player/Player.java
@@ -1,2 +1,20 @@
-package tictactoe.player;public class Player {
+
+package tictactoe.player;
+
+import tictactoe.TicTacToeSymbol;
+
+import java.awt.*;
+
+public abstract class Player {
+    private final TicTacToeSymbol identifier;
+
+    public Player(TicTacToeSymbol identifier) {
+        this.identifier = identifier;
+    }
+
+    public abstract Point choosePosition();
+
+    public TicTacToeSymbol getIdentifier() {
+        return identifier;
+    }
 }
